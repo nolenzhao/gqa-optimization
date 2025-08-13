b88afe7a90a440b47abcbc73090a780a18b1811f
Saaniya Saraf <saaniya.saraf@gmail.com>
2025-08-13 19:06:04 +0000

    removing redundant func defns

--- include/kernels.h
+++ include/kernels.h
@@ -34,15 +34,6 @@ namespace Mfma4x4PingPong {
 
     __global__ void gqa_packed(float16_t const* queries, float16_t const* key_mat, float32_t* attention_output, int group_size, int seq_len, int hidden_dim, int lda, int ldb, int ldd);
 
-    __device__ AFragT load_queries_4x4_col_major(float16_t const* input, int ld, int wave_id);
-
-    __device__ BFragT load_keys_4x4_row_major(float16_t const* input, int ld);
-
-    __device__ void store_attention_pattern_4x4_col_major(float32_t* output, Mfma4x4::AccumFragT accum, int ld);
-
-    __device__ void load_queries(float16_t* dst, float16_t const* src, int ld);
-
-    __device__ void load_keys_quad(float16_t* dst, float16_t const* src, int ld);
 }
 
 namespace Mfma4x4HalfLDS{
